653841a080d8db0b2a23737457f646df586c1346
Alex Harris <ajharris8814@gmail.com>
2017-09-18 20:34:41 -0400

    Lock Landscape Orientation for Main Activity

--- app/src/main/java/com/phototap/alexharris/unrotator/MainActivity.java
+++ app/src/main/java/com/phototap/alexharris/unrotator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.phototap.alexharris.unrotator;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 }
